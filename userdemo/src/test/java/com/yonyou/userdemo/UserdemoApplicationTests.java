99743360151ace19a74ed5ebf5f0f12082454ded
chenkangt <chenkangt@yonyou.com>
2019-02-28 21:12:39 +0800

    单元测试

--- userdemo/src/test/java/com/yonyou/userdemo/UserdemoApplicationTests.java
+++ userdemo/src/test/java/com/yonyou/userdemo/UserdemoApplicationTests.java
@@ -1,16 +1,43 @@
 package com.yonyou.userdemo;
 
+import com.yonyou.userdemo.controller.ContactController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class UserdemoApplicationTests {
+
+    private MockMvc mvc;
+
+
 
     @Test
     public void contextLoads() {
+    }
+
+    @Before
+    public void setUp(){
+        mvc = MockMvcBuilders.standaloneSetup(new ContactController()).build();
+    }
+
+    @Test
+    public void index() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/contact/failed").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk()).andExpect(content().string(equalTo("{\"msg\":\"无效的url!\",\"status\":0}")));
     }
 
 }
